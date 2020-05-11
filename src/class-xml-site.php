a3ddc4368b6497a90d7e0e2b60d24c04becbb1b1
ben <bhoogter@yahoo.com>
2020-05-11 19:38:38 -0400

    Extensions wip

--- src/class-xml-site.php
+++ src/class-xml-site.php
@@ -1,59 +1,74 @@
 <?php
 
-class xml_site extends source
+class xml_site 
 {
-    public $resource_folder;
-    public $source;
+    public static $resource_folder;
+    public static $source;
 
-    public function __construct()
+    public static function init()
     {
         $n = func_num_args();
         $a = func_get_args();
         php_logger::log("CONSTRUCT", $n, $a);
-        $this->resource_folder = $n >= 1 ? $a[0] : null;
-        php_logger::log("FOLDER=" . $this->resource_folder);
+        self::$resource_folder = $n >= 1 ? $a[0] : null;
+        php_logger::log("FOLDER=" . self::$resource_folder);
 
-        $this->init_source();
+        self::init_source();
         xml_serve::init(
-            $this->resource_folder,
-            dirname($this->resource_folder),
-            $this->get_source("PAGES"),
-            $this->get_source("SITE")
+            realpath(self::$resource_folder),
+            realpath(dirname(self::$resource_folder)),
+            self::$source->get_source("PAGES"),
+            self::$source->get_source("SITE")
         );
+        self::load_modules();
     }
 
-    protected function init_source()
+    public static function resolve_files($resource, $types = [], $mappings = [], $subfolders = ['.', '*']) { return xml_serve::resource_resolver()->resolve_files($resource, $types, $mappings, $subfolders); }
+    public static function resolve_refs($resource, $types = [], $mappings = [], $subfolders = ['.', '*']) { return xml_serve::resource_resolver()->resolve_refs($resource, $types, $mappings, $subfolders); }
+    public static function resolve_file($resource, $types = [], $mappings = [], $subfolders = ['.', '*']) { return xml_serve::resource_resolver()->resolve_file($resource, $types, $mappings, $subfolders); }
+    public static function resolve_ref($resource, $types = [], $mappings = [], $subfolders = ['.', '*']) { return xml_serve::resource_resolver()->resolve_ref($resource, $types, $mappings, $subfolders); }
+    public static function content_type($filename) { return xml_serve::resource_resolver()->content_type($filename); }
+
+    protected static function init_source()
     {
         php_logger::log("CALL");
-        $this->source = new source();
-        $this->add_source("SITE", $this->resource_folder . '/site.xml');
-        $this->add_source("PAGES", $this->resource_folder . '/pages.xml');
+        self::$source = new source();
+        self::$source->add_source("SITE", self::$resource_folder . '/site.xml');
+        self::$source->add_source("PAGES", self::$resource_folder . '/pages.xml');
+    }
 
+    protected function load_modules() 
+    {
         $modules = new xml_file();
-        $modules->merge($this->resource_folder . "/content/modules/*/module.xml", "modules", "module", realpath($this->resource_folder . "/content/generated/modules.xml"));
-        $this->add_source("MODULES", $modules);
-        $this->read_modules();
-        $this->include_startup_files();
+        $f = self::resolve_files("module.xml", [], [], ["modules/*"]);
+        php_logger::debug("DETECTED MODULES", $f);
+        $modules->merge($f, "modules", "module", realpath(self::$resource_folder . "/content/generated/modules.xml"));
+
+        self::$source->add_source("MODULES", $modules);
+        self::read_modules();
+        self::include_startup_files();
     }
 
     protected function read_modules()
     {
-        $modules = $this->nds("//MODULES/modules/module");
+        $modules = self::$source->nds("//MODULES/modules/module");
+        php_logger::dump("MODULES: ", $modules);
         foreach ($modules as $m) {
-            $module = xml_file::nodeXmlFile($m);
-            $elements = $module->nds("/specification/components/element");
+            $module = xml_file::toXmlFile($m);
+            $elements = $module->nds("/module/specification/components/element");
             foreach ($elements as $e) {
                 $name = $e->getAttribute("name");
                 $render = $e->getAttribute("render");
                 $src = $e->getAttribute("src");
-                page_render::add_handler($render, $src);
+                php_logger::trace("ADD HANDLER", $name, $render, $src);
+                xml_serve::add_handler($name, "$render::render");
             }
         }
     }
 
     function include_startup_files()
     {
-        return $this->include_support_files('', '', 'startup', '');
+        return self::include_support_files('', '', 'startup', '');
     }
 
     function include_support_files($module = '', $type = 'php', $mode = '', $file_id = "")
@@ -64,21 +79,20 @@ class xml_site extends source
         if ($module && $module != "") $p .= "[@module='$module']";
         if ($type && $type != "")   $p .= "[@type='$type']";
         if ($mode && $mode != "")   $p .= "[@mode='$mode']";
-        $files = $this->nds($p);
+        $files = self::$source->nds($p);
+        php_logger::dump("support files: ", $files);
 
         foreach ($files as $ff) {
             $src = $ff->getAttribute("src");
             $module = $ff->parentNode->getAttribute("name");
-            $f = $this->resolve_file($src, "module", $module);
+            $f = self::resolve_file($src, "module", $module);
             if ($f == "") throw new Exception("Could not find file for module.  Module=$module, src=$src");
-            print "\n=========\nsrc=$src, module=$module, f=$f\n";
+            // print "\n=========\nsrc=$src, module=$module, f=$f\n";
             switch ($type) {
-                case 'css':
-                    break;
-                case 'js':
-                    break;
+                case 'css': break;
+                case 'js':break;
                 default:
-                    php_logger::debug("SUPPORT FILE: including PHP file: $f");
+                    php_logger::debug("SUPPORT FILE: $f");
                     include_once($f);
                     break;
             }
@@ -88,20 +102,12 @@ class xml_site extends source
     function get_styles()
     {
         $s = '<list>';
-        foreach ($this->nds("//SYS/*/file[@type='css']") as $n) {
+        foreach (self::$source->nds("//SYS/*/file[@type='css']") as $n) {
             $f = $n->getAttribute('src');
             $m = $n->getAttribute('module');
             $s .= "<link type='text/css' rel='stylesheet' href='" . ExtendURL(juniper_module_url("$m/$f"), '', true) . "' />\n";
         }
         $s .= "</list>";
-        //print $s;
         return xml_file::XMLToDoc($s);
-    }
-
-    public function render($index)
-    {
-        $page = $this->server->get_page($index);
-        print $page;
-        return $page;
     }
 }
--- tests/resources/content/modules/adjunct/adjunct.php
+++ tests/resources/content/modules/adjunct/adjunct.php
@@ -1,1 +1,13 @@
+<?php
 
+class adjunct
+{
+    public static function render($el)
+    {
+        php_logger::log("CALL");
+        // $id = $el->getAttribute("id");
+        // if (!$id) $id = 'content';
+
+        return xml_serve::xml_content("<h3>ADJUNCT</h3>");
+    }
+}
