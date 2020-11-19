c5f5fc9cb7c8d229045e0059d9717cc004c2b635
Benjamin <bhoogter@yahoo.com>
2020-11-19 12:26:17 -0500

    Auth wip

--- demo/content/modules/auth/auth_login.php
+++ demo/content/modules/auth/auth_login.php
@@ -4,6 +4,23 @@ class auth_login
 {
     public static function render($el)
     {
-        return xml_serve::xml_content("<h3>LOGIN PAGE RENDERER</h3>");
+        $n = "\n";
+
+        $s  = "";
+        $s .= $n . '<style>';
+        $s .= $n . '';
+        $s .= $n . '</style>';
+        $s .= $n . '';
+        $s .= $n . '<div class="form-group">';
+        $s .= $n . '  <div class="col-sm-12">';
+        $s .= $n . '    <input type="text" class="form-control" id="userName" name="userName" spellcheck="false" autocomplete="off" maxlength="20" placeholder="USERID" required="" value="" />';
+        $s .= $n . '  </div>';
+        $s .= $n . '  <div class="col-sm-12">';
+        $s .= $n . '  <input class="form-control" type="password" id="password" name="password" placeholder="PASSWORD" required="" value="" />';
+        $s .= $n . '  </div>';
+        $s .= $n . '  </div>';
+        $s .= $n . '</div>"';
+
+        return xml_serve::xml_content($s);
     }
 }
